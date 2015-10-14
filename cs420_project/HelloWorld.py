93be9fe377adb8d370dc8d93cfe6a40ccf39aab0
Derek <djb3718@rit.edu>
2015-10-14 15:26:49 -0400

    Created HelloWorld.py to test subversion

---
+++ cs420_project/HelloWorld.py
@@ -0,0 +1,12 @@
+'''
+HelloWorld.py
+
+@author    Derek Brown <djb3718@rit.edu>
+Purpose    Test subversion
+'''
+
+def main():
+    print("Hello World!")
+    
+if __name__ == "__main__":
+    main()
